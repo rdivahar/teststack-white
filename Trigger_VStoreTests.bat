7c87cb6a70f210cea5541ca6687e48fb34e2cb4d
sdivahar <sdivahar@jestais.com>
2016-11-01 12:22:57 -0400

    Changing the shared drive (U:) to local drive for jenkins test.

--- Trigger_VStoreTests.bat
+++ Trigger_VStoreTests.bat
@@ -1,1 +1,1 @@
-C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Automation\bin\Debug\VStoreAutomation.dll --work="U:\VisionStoreAutomation\Results" --result=JenkinsDemo.xml
+C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Automation\bin\Debug\VStoreAutomation.dll --work="C:\Temp\VisionStoreAutomation\Results" --result=JenkinsDemo.xml
