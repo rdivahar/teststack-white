796caa667a5a1165590d819eeabd5ffb264ff508
sdivahar <sdivahar@jestais.com>
2016-10-31 21:08:40 -0400

    Removing the Folder hierarchy

--- Triggers/Trigger_VStoreTests.bat
+++
@@ -1,1 +0,0 @@
-C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Packages\NUnit.ConsoleRunner.3.5.0\tools\nunit3-console.exe C:\Jenkins\jobs\Jesta_DemoJob\workspace\VisionStore\Automation\bin\Debug\VStoreAutomation.dll --work="U:\VisionStoreAutomation\Results" --result=JenkinsDemo.xml
