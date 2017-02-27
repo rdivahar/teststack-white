b7896daa153bde9f162f52d7b87fffac403c06be
sdivahar <sdivahar@jestais.com>
2017-02-27 11:26:54 -0500

    Adding Test Initialize Hook

---
+++ VisionStore/Automation/TestsInitializer/TestInitializeHook.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Jesta.VStore.Automation.Framework.CommonLibrary;
+using Jesta.VStore.Automation.Framework.Configuration;
+using System.Xml.Linq;
+
+namespace Jesta.Automation.VisionStore.Tests
+{
+
+    [SetUpFixture]
+    public class TestInitializeHook : CommonUtility
+    {
+        
+        [OneTimeSetUp] [PreTest]
+        public void RunBeforeAnySuite()
+        {
+            string sTestSuiteName = CommonData.sDefaultSuite;
+            string sGetParameterName = TestContext.Parameters["Suite"];
+
+            if (sGetParameterName != null)
+              {
+                sTestSuiteName = sGetParameterName;
+              }
+            LoggerUtility.WriteLog("<Info> : The Name Of The TestSuite Passed - " + sTestSuiteName);
+            base.ConfigXMLWithTestSuiteName(sTestSuiteName);
+            LoggerUtility.SetupReportConfig(sTestSuiteName);
+        }
+
+        [OneTimeTearDown][PostTest]
+        public void RunAfterAnySuite()
+        {
+            LoggerUtility.WriteLog("Divahar & Dayamina");
+            LoggerUtility.FlushResultsAndClose();
+        }
+    }
+}
